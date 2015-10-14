2c7c6c9b6db31916dff5a1a98a3355f4071fd981
Ben <benj2240@gmail.com>
2015-10-14 06:31:51 -0500

    Removing initialization tests - they are covered by list insert tests

--- linked_list_tests.rb
+++ linked_list_tests.rb
@@ -2,19 +2,6 @@ require 'test/unit'
 require_relative 'linked_list.rb'
 
 class LinkedListTests < Test::Unit::TestCase
-	def test_initialize_empty_node
-		empty = EmptyNode.instance
-	end
-
-	def test_initialize_leaf_node
-		leaf = LeafNode.new 8
-	end
-
-	def test_initialize_link_node
-		leaf = LeafNode.new "j"
-		link = LinkNode.new "i", leaf
-	end
-
 	def test_empty_insert_returns_leaf
 		empty = EmptyNode.instance
 		expected = LeafNode.new :a
