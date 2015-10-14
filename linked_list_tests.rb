1df54454b42932256957ad11947fde514696d2be
Ben <benj2240@gmail.com>
2015-10-14 06:39:00 -0500

    Changing LeafNode.to_a test to a LinkedList.to_a test

--- linked_list_tests.rb
+++ linked_list_tests.rb
@@ -9,13 +9,6 @@ class LinkedListTests < Test::Unit::TestCase
 		assert_equal expected, result
 	end
 
-	def test_leaf_insert_returns_link
-		leaf = LeafNode.new -2
-		expected = LinkNode.new(-2, LeafNode.new([]))
-		result = leaf.insert []
-		assert_equal expected, result
-	end
-
 	def test_link_insert_returns_self
 		leaf = LeafNode.new 3
 		original_link = leaf.insert 2
@@ -38,9 +31,10 @@ class LinkedListTests < Test::Unit::TestCase
 		assert_equal [], empty.to_a
 	end
 
-	def test_leaf_node_to_a
-		leaf = LeafNode.new "f"
-		assert_equal ["f"], leaf.to_a
+	def test_single_element_to_a
+		list = LinkedList.new
+		list.insert "f"
+		assert_equal ["f"], list.to_a
 	end
 
 	def test_link_node_to_a
