427c94a3fd8663827cdf2c7d3ee0de88e8361125
Ben <benj2240@gmail.com>
2015-10-14 06:54:56 -0500

    Splitting up a test with multiple asserts

--- linked_list_tests.rb
+++ linked_list_tests.rb
@@ -138,15 +138,28 @@ class LinkedListTests < Test::Unit::TestCase
 		assert_equal expected, result
 	end
 
-	def test_delete_from_list
+	def test_delete_item_not_in_list
 		list = LinkedList.new [1,2,3,4]
 		assert_raise RuntimeError do
 			list.delete 5
 		end
-		assert_equal [1,2,3,4], list.to_a
+	end
+
+	def test_delete_from_end_of_list
+		list = LinkedList.new [1,2,3,4]
 		list.delete 4
 		assert_equal [1,2,3], list.to_a
+	end
+
+	def test_delete_from_middle_of_list
+		list = LinkedList.new [1,2,3]
 		list.delete 2
 		assert_equal [1,3], list.to_a
+	end
+
+	def test_delete_from_start_of_list
+		list = LinkedList.new [1,2,3]
+		list.delete 1
+		assert_equal [2,3], list.to_a
 	end
 end
